abbbdd62d982edc0094ed3dd1db9e4ebcbce41dd
Kayahan Baskeser <kayahan.baskeser@gmail.com>
2021-04-27 01:46:56 +0300

    updated to v2.0.3

--- Assets/Huawei/Scripts/Utils/HMSLeaderboardConstants.cs
+++ Assets/Huawei/Scripts/Utils/HMSLeaderboardConstants.cs
@@ -1,4 +1,4 @@
 public class HMSLeaderboardConstants
 {
-	public const string fsdfsdfs = "fsdfs";
+	public const string LeaderBoard = "D31B8DE026592F877303293ADFDE703CDFFA5D8E7FAA2C7E568AA935C684548D";
 }
